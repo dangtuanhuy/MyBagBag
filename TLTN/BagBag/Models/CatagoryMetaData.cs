96dc124a63bd874b94cd914450d690e294a0a135
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-09-24 22:33:47 +0700

    data

---
+++ TLTN/BagBag/Models/CatagoryMetaData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace BagBag.Models
+{
+    [MetadataType(typeof(Category.CatagoryMetaData))]
+    public partial class Category
+    {
+        internal sealed class CatagoryMetaData
+        {
+            public int CategoryId { get; set; }
+            [Display(Name ="Danh Mục Sản Phẩm")]
+            public string CategoryName { get; set; }
+            [Display(Name = "Mô tả")]
+            public string CategoryDetails { get; set; }
+        }
+    }
+   
+}
