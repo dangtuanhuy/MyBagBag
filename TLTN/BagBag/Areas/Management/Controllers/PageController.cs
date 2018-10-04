4cb2877c7f27b18493d84cef26da9c768eee98cc
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-10-04 09:36:54 +0700

    GUI

--- TLTN/BagBag/Areas/Management/Controllers/PageController.cs
+++ TLTN/BagBag/Areas/Management/Controllers/PageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BagBag.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,41 @@ namespace BagBag.Areas.Management.Controllers
 {
     public class PageController : Controller
     {
+        private MyBagBagEntities db = new MyBagBagEntities();
         // GET: Management/Page
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult _statusPromotion()
+        {
+            var statusPromotion = (from p in db.Promotions where p.PromotionStatus == true select p).Count();
+            ViewBag.statusPromotion = statusPromotion;
+            return PartialView();
+        }
+        public ActionResult _countProducts()
+        {
+            var countProducts = db.Products.Count();
+            ViewBag.countProducts = countProducts;
+            return PartialView();
+        }
+        public ActionResult _countContacts()
+        {
+            var statusContacts = (from p in db.Contacts where p.Status == false select p).Count();
+
+            ViewBag.statusContacts = statusContacts;
+            return PartialView();
+        }
+        public ActionResult _countCart()
+        {
+            var countCarts = (from p in db.Orders select p).Count();
+
+            ViewBag.countCarts = countCarts;
+            return PartialView();
+        }
+        public ActionResult _FooterPage()
+        {
+            return PartialView("_FooterPage");
         }
     }
 }
