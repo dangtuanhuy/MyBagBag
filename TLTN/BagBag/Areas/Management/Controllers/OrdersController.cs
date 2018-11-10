d7a3f9fef151a8e804fec376a4287f1ed0670d73
Dang Tuan Huy <huyb1505883@student.ctu.edu.vn>
2018-11-10 10:13:45 +0700

    TL

--- TLTN/BagBag/Areas/Management/Controllers/OrdersController.cs
+++ TLTN/BagBag/Areas/Management/Controllers/OrdersController.cs
@@ -83,6 +83,7 @@ namespace BagBag.Areas.Management.Controllers
         // POST: Management/Orders/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "OrderId,CustomerCode,OrderDate,RequiredDate,OrderAddress,OrderPhone,PaymentMethod,PaymentOnline,Order_Status")] Order order)
